05bf47644de765d74bf7d19bf2576aa76c5abc3f
Lucas Henrique <lukas.paes18@gmail.com>
2022-12-28 01:52:04 -0300

    feature: variaveis

---
+++ variaveis-python.py
@@ -0,0 +1,18 @@
+# Variaveis
+
+#Numero
+velocidade_maxima = 240
+print(velocidade_maxima)
+
+#Text
+frase_boas_vindas = "Olá"
+print(frase_boas_vindas)
+
+#Booleans
+entrar_casa = False
+
+#Decimal
+dollar = 5.25
+
+##Tipo Variavel
+print(type(velocidade_maxima))
